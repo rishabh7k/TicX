331dbe87e4c5932351973dacf50190af17c805c9
rishabh <rishabh6286@gmail.com>
2024-08-27 22:49:04 +0530

    added navbar, dashboard components/

---
+++ tixly/app/dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex">
+      <div className="h-svh w-24 border"></div>
+      <div className="w-full border"></div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ tixly/app/navbar/navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import { ImageComponents } from "../images/component";
+const Navbar = () => {
+  return (
+    <div className="flex font-bold h-16">
+      <div
+        className="text-white font-extrabold text-2xl text-center p-3"
+        style={{ background: "#5d77ff", width: "91px" }}
+      >
+        TicX
+      </div>
+    </div>
+  );
+};
+export default Navbar;
